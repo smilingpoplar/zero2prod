f800677cb58cb5c534b87e934054e9eb8503ca17
le <smilingpoplar@gmail.com>
2023-10-31 17:17:59 +0800

    6.13.4

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -47,9 +47,25 @@ mod tests {
         assert_err!(SubscriberEmail::parse(email));
     }
 
-    #[test]
-    fn valid_emails_are_parsed_successfully() {
-        let email = SafeEmail().fake();
-        claim::assert_ok!(SubscriberEmail::parse(email));
+    // #[test]
+    // fn valid_emails_are_parsed_successfully() {
+    //     let email = SafeEmail().fake();
+    //     claim::assert_ok!(SubscriberEmail::parse(email));
+    // }
+
+    // Both `Clone` and `Debug` are required by `quickcheck`
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
