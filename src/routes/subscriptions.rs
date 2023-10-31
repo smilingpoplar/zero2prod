1c0a26d6179eb2c7dd195ca1ed5bbefc57073d03
le <smilingpoplar@gmail.com>
2023-10-31 17:38:01 +0800

    6.14

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,7 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberName, SubscriberEmail};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -26,10 +26,11 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         // Return early if the name is invalid, with a 400 
         Err(_) => return HttpResponse::BadRequest().finish(), 
     };
-    let new_subscriber = NewSubscriber { 
-        email: form.0.email, 
-        name,
+    let email = match SubscriberEmail::parse(form.0.email) { 
+        Ok(email) => email, 
+        Err(_) => return HttpResponse::BadRequest().finish(), 
     };
+    let new_subscriber = NewSubscriber { email, name };
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish()
@@ -47,7 +48,7 @@ pub async fn insert_subscriber(
     sqlx::query!(
         r#" INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4) "#,
         Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
     )
