7449f817b62d217aecb733d751ebd8cb26b1249a
le <smilingpoplar@gmail.com>
2023-11-07 15:10:37 +0800

    7.7.6

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -5,6 +5,8 @@ use uuid::Uuid;
 use crate::domain::{NewSubscriber, SubscriberName, SubscriberEmail};
 use crate::email_client::EmailClient;
 use crate::startup::ApplicationBaseUrl;
+use rand::distributions::Alphanumeric; 
+use rand::{thread_rng, Rng};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -42,15 +44,23 @@ pub async fn subscribe(
         Ok(subscriber) => subscriber, 
         Err(_) => return HttpResponse::BadRequest().finish(), 
     };
-    if insert_subscriber(&pool, &new_subscriber).await.is_err() {
-        return HttpResponse::InternalServerError().finish()
+    let subscriber_id = match insert_subscriber(&pool, &new_subscriber).await {
+        Ok(subscriber_id) => subscriber_id,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
+    let subscription_token = generate_subscription_token();
+    if store_token(&pool, subscriber_id, &subscription_token)
+        .await
+        .is_err() 
+    {
+        return HttpResponse::InternalServerError().finish(); 
     }
 
-    // Pass the application url 
     if send_confirmation_email( 
         &email_client, 
         new_subscriber, 
-        &base_url.0 
+        &base_url.0,
+        &subscription_token,
     )
     .await 
     .is_err()
@@ -67,11 +77,12 @@ pub async fn subscribe(
 pub async fn insert_subscriber( 
     pool: &PgPool, 
     new_subscriber: &NewSubscriber,
-) -> Result<(), sqlx::Error> {
+) -> Result<Uuid, sqlx::Error> {
+    let subscriber_id = Uuid::new_v4();
     sqlx::query!(
         r#" INSERT INTO subscriptions (id, email, name, subscribed_at, status) 
         VALUES ($1, $2, $3, $4, 'pending_confirmation') "#,
-        Uuid::new_v4(),
+        subscriber_id,
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
@@ -82,20 +93,25 @@ pub async fn insert_subscriber(
         tracing::error!("Failed to execute query: {:?}", e);
         e
     })?; 
-    Ok(())
+    Ok(subscriber_id)
 }
 
 #[tracing::instrument( 
     name = "Send a confirmation email to a new subscriber", 
-    skip(email_client, new_subscriber, base_url) 
+    skip(email_client, new_subscriber, base_url, subscription_token) 
 )]
 pub async fn send_confirmation_email(
     email_client: &EmailClient, 
     new_subscriber: NewSubscriber, 
     base_url: &str,
+    subscription_token: &str,
 ) -> Result<(), reqwest::Error> {
     // Build a confirmation link with a dynamic root 
-    let confirmation_link = format!("{}/subscriptions/confirm?subscription_token=mytoken", base_url); 
+    let confirmation_link = format!(
+        "{}/subscriptions/confirm?subscription_token={}", 
+        base_url, 
+        subscription_token
+    ); 
     let plain_body = format!( "Welcome to our newsletter!\nVisit {} to confirm your subscription.", confirmation_link ); 
     let html_body = format!( 
         "Welcome to our newsletter!<br />\
@@ -110,4 +126,37 @@ pub async fn send_confirmation_email(
             &plain_body,        
         )
         .await
+}
+
+/// Generate a random 25-characters-long case-sensitive subscription token. 
+fn generate_subscription_token() -> String {
+    let mut rng = thread_rng();
+    std::iter::repeat_with(|| rng.sample(Alphanumeric)) 
+        .map(char::from) 
+        .take(25) 
+        .collect() 
+}
+
+#[tracing::instrument( 
+    name = "Store subscription token in the database", 
+    skip(pool, subscription_token) 
+)] 
+pub async fn store_token( 
+    pool: &PgPool, 
+    subscriber_id: Uuid, 
+    subscription_token: &str, 
+) -> Result<(), sqlx::Error> { 
+    sqlx::query!( 
+        r#"INSERT INTO subscription_tokens (subscription_token, subscriber_id) 
+        VALUES ($1, $2)"#, 
+        subscription_token, 
+        subscriber_id 
+    ) 
+    .execute(pool) 
+    .await
+    .map_err(|e| { 
+        tracing::error!("Failed to execute query: {:?}", e); 
+        e 
+    })?; 
+    Ok(())
 }
