354a02f31a9ab9678af65cac3f17177c3e4b184b
le <smilingpoplar@gmail.com>
2023-11-09 14:15:09 +0800

    8.2.2

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -167,7 +167,6 @@ pub async fn store_token(
     Ok(())
 }
 
-#[derive(Debug)]
 pub struct StoreTokenError(sqlx::Error);
 
 impl ResponseError for StoreTokenError {}
@@ -180,4 +179,30 @@ impl std::fmt::Display for StoreTokenError {
             trying to store a subscription token."
         ) 
     }
+}
+
+impl std::error::Error for StoreTokenError { 
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> { 
+        // The compiler transparently casts `&sqlx::Error` into a `&dyn Error` 
+        Some(&self.0) 
+    } 
+}
+
+fn error_chain_fmt( 
+    e: &impl std::error::Error, 
+    f: &mut std::fmt::Formatter<'_>, 
+) -> std::fmt::Result { 
+    writeln!(f, "{}\n", e)?; 
+    let mut current = e.source(); 
+    while let Some(cause) = current { 
+        writeln!(f, "Caused by:\n\t{}", cause)?; 
+        current = cause.source(); 
+    } 
+    Ok(())
+}
+
+impl std::fmt::Debug for StoreTokenError { 
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result { 
+        error_chain_fmt(self, f)
+    } 
 }
