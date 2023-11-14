552522bf103991db2106c693148aee7fa0702062
le <smilingpoplar@gmail.com>
2023-11-14 18:16:13 +0800

    9.5

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,6 +1,20 @@
+use actix_web::web;
 use actix_web::HttpResponse;
 
-// Dummy implementation
-pub async fn publish_newsletter() -> HttpResponse {
+#[derive(serde::Deserialize)]
+pub struct BodyData {
+    title: String,
+    content: Content,
+}
+
+#[derive(serde::Deserialize)]
+pub struct Content {
+    html: String,
+    text: String,
+}
+
+// We are prefixing `body` with a `_` to avoid
+// a compiler warning about unused arguments
+pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
