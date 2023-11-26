488c74dbad6db946fe0984a8ad88662b324e76d4
le <smilingpoplar@gmail.com>
2023-11-26 18:10:34 +0800

    10.7.5.2

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -7,6 +7,7 @@ use crate::routes::error_chain_fmt;
 use actix_web::web;
 use actix_web::error::InternalError;
 use actix_web_flash_messages::FlashMessage;
+use actix_session::Session;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -15,10 +16,10 @@ pub struct FormData {
 }
 
 #[tracing::instrument( 
-    skip(form, pool ), 
+    skip(form, pool, session ), 
     fields(username=tracing::field::Empty, user_id=tracing::field::Empty) 
 )] 
-pub async fn login(form: web::Form<FormData>, pool: web::Data<PgPool>,) 
+pub async fn login(form: web::Form<FormData>, pool: web::Data<PgPool>, session: Session,) 
         -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials { 
         username: form.0.username, password: form.0.password, 
@@ -29,9 +30,13 @@ pub async fn login(form: web::Form<FormData>, pool: web::Data<PgPool>,)
         Ok(user_id) => {
             tracing::Span::current()
                 .record("user_id", &tracing::field::display(&user_id));
-            
+
+            session.renew();
+            session
+                .insert("user_id", user_id)
+                .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
             Ok(HttpResponse::SeeOther()
-                .insert_header((LOCATION, "/")) 
+                .insert_header((LOCATION, "/admin/dashboard"))
                 .finish())
         }
         Err(e) => {
@@ -39,11 +44,7 @@ pub async fn login(form: web::Form<FormData>, pool: web::Data<PgPool>,)
                 AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()), 
                 AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()), 
             };
-            FlashMessage::error(e.to_string()).send();
-            let response = HttpResponse::SeeOther() 
-                .insert_header((LOCATION, "/login"))
-                .finish();
-            Err(InternalError::from_response(e, response))
+            Err(login_redirect(e))
         }
     }
 }
@@ -62,3 +63,11 @@ impl std::fmt::Debug for LoginError {
     } 
 }
 
+// Redirect to the login page with an error message.
+fn login_redirect(e: LoginError) -> InternalError<LoginError> { 
+    FlashMessage::error(e.to_string()).send(); 
+    let response = HttpResponse::SeeOther() 
+        .insert_header((LOCATION, "/login")) 
+        .finish(); 
+    InternalError::from_response(e, response) 
+}
--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,6 @@
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
+use crate::routes::admin_dashboard;
 use crate::routes::confirm;
 use crate::routes::{health_check, publish_newsletter, subscribe};
 use crate::routes::{home, login, login_form};
@@ -119,6 +120,7 @@ async fn run(
             .route("/", web::get().to(home))
             .route("/login", web::get().to(login_form))
             .route("/login", web::post().to(login))
+            .route("/admin/dashboard", web::get().to(admin_dashboard))
             // Register the connection as part of the application state
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
