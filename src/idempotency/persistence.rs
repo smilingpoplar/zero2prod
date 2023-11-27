5312b94fc45b77c4ed77c7d91fcb13dbf148e33b
le <smilingpoplar@gmail.com>
2023-11-28 00:05:25 +0800

    11.9.2.1

--- src/idempotency/persistence.rs
+++ src/idempotency/persistence.rs
@@ -4,6 +4,7 @@ use actix_web::http::StatusCode;
 use actix_web::HttpResponse;
 use sqlx::postgres::PgHasArrayType;
 use sqlx::PgPool;
+use sqlx::{Postgres, Transaction};
 use uuid::Uuid;
 
 #[derive(Debug, sqlx::Type)]
@@ -48,7 +49,7 @@ pub async fn get_saved_response(
 }
 
 pub async fn save_response(
-    pool: &PgPool,
+    mut transaction: Transaction<'static, Postgres>,
     idempotency_key: &IdempotencyKey,
     user_id: Uuid,
     http_response: HttpResponse,
@@ -85,8 +86,9 @@ pub async fn save_response(
         headers,
         body.as_ref()
     )
-    .execute(pool)
+    .execute(&mut transaction)
     .await?;
+    transaction.commit().await?;
 
     let http_response = response_head.set_body(body).map_into_boxed_body();
     Ok(http_response)
@@ -98,8 +100,10 @@ impl PgHasArrayType for HeaderPairRecord {
     }
 }
 
+#[allow(clippy::large_enum_variant)]
 pub enum NextAction {
-    StartProcessing,
+    // Return transaction for later usage
+    StartProcessing(Transaction<'static, Postgres>),
     ReturnSavedResponse(HttpResponse),
 }
 
@@ -108,6 +112,7 @@ pub async fn try_processing(
     idempotency_key: &IdempotencyKey,
     user_id: Uuid,
 ) -> Result<NextAction, anyhow::Error> {
+    let mut transaction = pool.begin().await?;
     let n_inserted_rows = sqlx::query!(
         r#" 
         INSERT INTO idempotency (
@@ -121,11 +126,11 @@ pub async fn try_processing(
         user_id,
         idempotency_key.as_ref()
     )
-    .execute(pool)
+    .execute(&mut transaction)
     .await?
     .rows_affected();
     if n_inserted_rows > 0 {
-        Ok(NextAction::StartProcessing)
+        Ok(NextAction::StartProcessing(transaction))
     } else {
         let saved_response = get_saved_response(pool, idempotency_key, user_id)
             .await?
